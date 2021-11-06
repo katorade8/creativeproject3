71195464d54b972c0c53c3b300821b563885a0af
Kade Greenburg <kadegreenburg@gmail.com>
2021-11-06 14:53:46 -0600

    should run

--- src/views/Photo.vue
+++ src/views/Photo.vue
@@ -1,17 +1,29 @@
 <template>
-  
+  <div class="img">
+    <img src="image.src">
+    <i>{{image.caption}}</i>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
+//import { defineComponent } from '@vue/composition-api'
 
-export default defineComponent({
-  setup() {
-    
-  },
-})
+export default {
+  name: 'Photo'
+}
 </script>
 
 <style scoped>
+
+.img {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+}
+
+img {
+  margin: 20px;
+}
 
 </style>
