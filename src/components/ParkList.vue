17da6d25ca281bc8d56a196775f283516149fb34
Brandon <bjpaul99@gmail.com>
2021-11-06 17:47:38 -0600

    aMade search bar and list of parks

---
+++ src/components/ParkList.vue
@@ -0,0 +1,108 @@
+<template>
+<div class="wrapper">
+  <div class="products">
+    <div class="product" v-for="park in parks" :key="park.id">
+      <div class="info">
+        <h1>{{park.fullName}}</h1>
+        <p>{{park.states}}</p>
+      </div>
+      <div class="image">
+        <!--<img :src="'/images/products/'+product.image">-->
+        <img :src='park.images[0].url'/>
+
+      </div>
+      <!--
+      <div class="price">
+        <h2>{{product.price}}</h2>
+
+
+        <button class="auto" @click='addToCart(product)'>Add to Cart</button>
+  
+      </div>
+      -->
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'ParkList',
+    props: {
+        parks: Array,
+    },
+}
+
+</script>
+
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.products {
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.product {
+  margin: 10px;
+  margin-top: 50px;
+  width: 200px;
+}
+
+.product img {
+  border: 2px solid #333;
+  height: 250px;
+  width: 200px;
+  object-fit: cover;
+}
+
+.product .image {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 5px;
+}
+
+.info {
+  background: #F2921D;
+  color: #000;
+  padding: 10px 30px;
+  height: 80px;
+}
+
+.info h1 {
+  font-size: 16px;
+}
+
+.info h2 {
+  font-size: 14px;
+}
+
+.info p {
+  margin: 0px;
+  font-size: 10px;
+}
+
+
+.price {
+  display: flex;
+}
+
+button {
+  height: 50px;
+  background: #000;
+  color: white;
+  border: none;
+}
+
+.auto {
+  margin-left: auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,75 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+
+  <div>
+
+    <div class="search">
+      <form class="pure-form">
+        <input type='text' v-model="searchText" />
+      </form>
+    </div>
+
+    <ParkList :parks='parks'/>
+
   </div>
+
 </template>
 
 <script>
+import ParkList from "../components/ParkList.vue"
 
 export default {
   name: 'Home',
   components: {
+    ParkList,
+  },
 
+  data() {
+    return {
+      searchText: '',
+    }
+  },
+
+  computed: {
+    parks() {
+      return this.$root.$data.parks.filter(park => park.fullName.toLowerCase().search(this.searchText.toLowerCase()) >= 0);
+    }
   }
+
 }
 </script>
+
+
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.search {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 50%;
+}
+
+form {
+  display: table;
+  width: 100%;
+}
+
+i {
+  display: table-cell;
+  padding-left: 10px;
+  width: 1px;
+}
+
+input {
+  display: table-cell;
+  font-size: 20px;
+  border: none !important;
+  box-shadow: none !important;
+  width: 100%;
+  height: 40px;
+}
+</style>
