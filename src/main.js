3c2bd0cf2d8189022b3f0b39f843e6979d107e74
Brandon <bjpaul99@gmail.com>
2021-11-06 16:24:02 -0600

    Added Array of parks

--- src/main.js
+++ src/main.js
@@ -4,7 +4,31 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+
+let tmp = []
+let url = 'https://developer.nps.gov/api/v1/parks?limit=465&api_key=NbIPOLabrmVaAyIClWYjTbBVpme58IAaB2pNKkHl'
+fetch(url)
+    .then(response => {
+      return response.json()
+}).then(json => {
+      console.log(json)
+      
+      for (let i = 0; i < json.data.length; i++) {
+        if (json.data[i].designation == "National Park") {
+          tmp.push(json.data[i])
+        }
+      }
+
+})
+
+let data = {
+  parks: tmp,
+}
+
+
+
 new Vue({
   router,
+  data,
   render: h => h(App)
 }).$mount('#app')
--- src/views/ThingsToDo.vue
+++ src/views/ThingsToDo.vue
@@ -5,10 +5,8 @@
 
 
 <script>
-//import { defineComponent } from '@vue/composition-api'
-
 export default {
 
-    
+
 }
 </script>
