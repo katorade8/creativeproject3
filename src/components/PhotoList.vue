f420989f394519407890677771e6e2a9e8434414
Kade Greenburg <kadegreenburg@gmail.com>
2021-11-06 22:11:12 -0600

    working PhotoList

---
+++ src/components/PhotoList.vue
@@ -0,0 +1,38 @@
+<template>
+<div class=img-list>
+  <div v-for="image in images" :key="image.url" class="image-w-caption">
+    <img class="photo" :src="image.url">
+    <p class=caption>{{image.caption}}</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'PhotoList',
+  props: {
+    images: Array
+  }
+}
+</script>
+
+<style scoped>
+.img-list {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  width: 100%;
+}
+
+.photo {
+  width: 60%;
+  border-radius: 10px;
+  justify-content: center;
+  margin-bottom: 10px;
+}
+
+.caption {
+  margin-bottom: 40px;
+}
+</style>
