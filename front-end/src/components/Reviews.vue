d61da8f36396ac705cd47b83582ee005d7d1480f
Brandon <bjpaul99@gmail.com>
2021-12-04 14:31:00 -0700

    Added Reviews Functionality

---
+++ front-end/src/components/Reviews.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>Component Review Data</h2>
+  </div>
+</template>
+
+
+
+
+
+<script>
+export default {
+
+  data() {
+    return {
+      review: {
+        content: '',
+        stars: 0,
+        person: '',
+
+      }
+    }
+  },
+
+  methods: {
+
+  },
+}
+</script>
+
+
+
+
+
+
+
+
+<style scoped>
+
+
+</style>
--- front-end/src/views/Reviews.vue
+++ front-end/src/views/Reviews.vue
@@ -1,5 +1,58 @@
 <template>
   <div>
     <h1>Reviews</h1>
+
+    <Reviews/>
+
+    <div class='reviewContainer'>
+      <div class='reviewBox'>
+        <p>Test</p>
+      </div>
+    </div>
+
   </div>
+
+
 </template>
+
+
+
+
+
+<script>
+import Reviews from '../components/Reviews.vue'
+
+export default {
+
+  components: {
+    Reviews,
+  },
+
+}
+
+</script>
+
+
+
+
+
+
+
+
+<style scoped>
+
+.reviewContainer {
+  display: flex;
+  justify-content: center;
+}
+
+
+.reviewBox {
+  display: flex;
+
+  background-color: lightblue;
+
+  width: 50%
+}
+
+</style>
